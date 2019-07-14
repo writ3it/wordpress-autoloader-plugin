e4196d1c7f05f85613e329d23ecd38dc935c3b82
writ3it <42292507+writ3it@users.noreply.github.com>
2019-07-14 16:44:25 +0200

    autoloading test

--- tests/wp-content/mu-plugins/AutoloaderTest.php
+++ tests/wp-content/mu-plugins/AutoloaderTest.php
@@ -15,7 +15,7 @@ require_once MU_PLUGINS.'/!!!autoloader.php';
 class AutoloaderTest extends TestCase
 {
     /**
-     * Autoloader must be loaded as fast as possible.
+     * Autoloader has to be loaded as fast as possible.
      * Sort function is a bottleneck in wordpress in this case.
      */
     public function test_sort_function(){
@@ -30,6 +30,15 @@ class AutoloaderTest extends TestCase
         sort($data);
         $first = array_values($data)[0];
         $this->assertEquals($autoloaderName,$first);
+    }
+
+    public function test_loading_success(){
+        $this->assertEquals(false, class_exists('\Test01Set\Test01'));
+        ___Autoloader::namespaces([
+            'Test01Set\\'=>__DIR__.'/examples/Test01Set'
+        ]);
+        $obj = new \Test01Set\Test01();
+        $this->assertEquals(true, class_exists('\Test01Set\Test01'));
     }
 
 }
---
+++ tests/wp-content/mu-plugins/examples/Test01Set/Test01.php
@@ -0,0 +1,13 @@
+<?php
+namespace Test01Set;
+/**
+ * Created by PhpStorm.
+ * User: writ3it
+ * Date: 14.07.19
+ * Time: 16:41
+ */
+
+class Test01
+{
+
+}
